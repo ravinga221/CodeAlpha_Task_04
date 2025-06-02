0a6141fc594ebbd98c8f321d55f7e0ea639c1d7d
Ravinga Deshani <147974756+ravinga221@users.noreply.github.com>
2025-06-03 00:36:26 +0530

    availableRoom

--- HotelReservation.java
+++ HotelReservation.java
@@ -2,5 +2,14 @@ import java.util.*;
 
 public class HotelReservation
 {
+	private static Map<Integer, Boolean> rooms = new HashMap<>();
 	
+	private static void viewAvailableRooms() {
+        System.out.println("\nAvailable Rooms:");
+        for (Map.Entry<Integer, Boolean> entry : rooms.entrySet()) {
+            if (entry.getValue()) {
+                System.out.println("Room " + entry.getKey());
+            }
+        }
+    }
 }
